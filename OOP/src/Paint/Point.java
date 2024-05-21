1b6934bd137d7347584b58b0420e935bca679805
Alexey Kiselev <alexey8837@gmail.com>
2024-05-21 21:33:26 +0300

    Сделано 4 задание, связанное с ООП

---
+++ OOP/src/Paint/Point.java
@@ -0,0 +1,22 @@
+package Paint;
+
+public class Point {
+    private double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public String print(){
+        return x + " " + y;
+    }
+}
